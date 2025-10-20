8884442dd36f9328a9683e861e2b170b88201856
Daniel Thrasher <160073182+Drthrasher84@users.noreply.github.com>
2025-10-20 23:42:51 +0000

    chore: dockerfile

--- src/api/posts.js
+++ src/api/posts.js
@@ -7,17 +7,18 @@ export const getPosts = async (queryParams) => {
 }
 
 export const createPost = async (token, post) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/
-posts`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(post),
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
-  )
+    body: JSON.stringify(post),
+  })
+  return await res.json()
+}
+
+export const getPostById = async (postId) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts/${postId}`)
   return await res.json()
 }
